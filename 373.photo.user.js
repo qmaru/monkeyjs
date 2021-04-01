ee6fa0a86ad66e703dad4e2630c2a7581a49b89e
unknown <csmcc2@hotmail.com>
2021-04-01 13:22:20 +0800

    compatible with outdated pages

--- 373.photo.user.js
+++ 373.photo.user.js
@@ -8,7 +8,7 @@
 // @grant       GM_addStyle
 // @grant       GM_setClipboard
 // @grant       GM_xmlhttpRequest
-// @version     1.0.0
+// @version     1.0.1
 // ==/UserScript==
 (function () {
     'use strict';
@@ -49,7 +49,7 @@
             var url = urls[i]
             var img_id = url["id"]
             var img_url = url["url"]
-            var img = '<li id="fc_li"><a href="' + img_url + '">' + img_id + '</a></li>'
+            var img = '<li id="fc_li"><a target="_blank" href="' + img_url + '">' + img_id + '</a></li>'
             imglist = imglist + img
         }
         var imgBody = '<ul id="fc_img"><li>Click to download</li>' + imglist + '</ul>'
@@ -64,9 +64,17 @@
         $("#fc_btn").click(function () {
             var imgs = []
             $(".gallery-photos div a").each(function () {
-                var img_id_raw = $(this).attr("id")
-                var img_id = img_id_raw.split("_")[1]
-                var dl_url = prefix_url + img_id + suffix_url
+                try {
+                    var img_id_raw = $(this).attr("id")
+                    var img_id = img_id_raw.split("_")[1]
+                    var dl_url = prefix_url + img_id + suffix_url
+                } catch {
+                    var dl_url = $(this).attr("href")
+                    var dl_url_parts = dl_url.split("/")
+                    var dl_url_name = dl_url_parts[dl_url_parts.length - 1]
+                    var img_id_name = dl_url_name.split("?")[0]
+                    var img_id = img_id_name.split("-")[0]
+                }
                 imgs.push({
                     "id": img_id,
                     "url": dl_url
